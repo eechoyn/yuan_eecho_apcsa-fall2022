94d13a75ca1bd34ff31f91699babff26eb2b4178
Eecho Yuan <eecho.yuan@gmail.com>
2022-08-19 13:02:25 -0700

    Unit02

---
+++ Unit02/src/Cube.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Eecho Yuan 
+//Date - 08/19/2022
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		return (6*side*side); 
+	}
+}
---
+++ Unit02/src/Trap.java
@@ -0,0 +1,13 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return 0.5*(double)height*(double)(base1+base2);
+	}
+}
