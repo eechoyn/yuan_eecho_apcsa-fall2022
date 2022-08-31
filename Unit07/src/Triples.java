c9f051d24aafb782d4ca1ff20a21c2e2a0577492
Eecho Yuan <eecho.yuan@gmail.com>
2022-08-31 12:23:55 -0700

    Unit07 Assignment

--- Unit07/src/Triples.java
+++ Unit07/src/Triples.java
@@ -15,34 +15,66 @@ public class Triples
 
 	public Triples(int num)
 	{
-
-
+		setNum(num); 
 	}
 
 	public void setNum(int num)
 	{
-
-
+		number = num; 
 	}
 	
+//	private int greatestCommonFactor(int a, int b, int c)
+//	{
+//		int max = 0;
+//		return 1;
+//	}
 	private int greatestCommonFactor(int a, int b, int c)
 	{
 		int max = 0;
-
-
-
-		return 1;
+		int factor; 
+		for (int i=1; i<=Math.min(a, Math.min(b, c)); i++) {
+			if(a%i==0 && b%i==0 && c%i==0) {
+				factor=i; 
+				if (Math.max(factor, max)==factor) {
+					max=factor; 
+				}
+			}
+		}
+		return max;
+	}
+	private boolean oddOrEven(int a, int b, int c) {
+		if (a%2==0 && b%2!=0 && c%2!=0) {
+			return true; 
+		}
+		else if (a%2!=0 && b%2==0 && c%2!=0) {
+			return true; 
+		}
+		else {
+			return false; 
+		}
+	}
+	private boolean theorem(int a, int b, int c) {
+		if (Math.pow((double)a, 2)+Math.pow((double)b, 2)==Math.pow((double)c, 2)) {
+			return true; 
+		}
+		else {
+			return false; 
+		}
 	}
 
 	public String toString()
 	{
 		String output="";
-
-
-
-
-
-
+		
+		for (int a =0; a<= number; a++) {
+			for (int b =0; b<= number; b++) {
+				for (int c = 0; c<=number; c++) {
+					if (greatestCommonFactor(a, b, c)==1 && oddOrEven(a,b,c) && theorem(a,b,c)) {
+						output = output + "\n " + a + " " + b + " " + c; 
+					}
+				}
+			}
+		}
 		return output+"\n";
 	}
 }
