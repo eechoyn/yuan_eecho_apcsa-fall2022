67b2cf067555c8017dbd06bc107997712376a598
Eecho Yuan <eecho.yuan@gmail.com>
2022-09-06 16:33:58 -0700

    Unit07

--- Unit07/src/TriangleFive.java
+++ Unit07/src/TriangleFive.java
@@ -11,23 +11,54 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		setLetter('A'); 
+		setAmount(0); 
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		setLetter(c); 
+		setAmount(amt); 
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c; 
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt; 
 	}
 
 	public String toString()
 	{
 		String output="";
+		char let = letter; 
+		int x = amount; 
+		int y = amount; 
+		
+		for (int i1=amount; i1>0; i1--) {
+			for (int i2=0; i2<i1; i2++) {
+				while(x>0) {
+					output += "" + let; 
+					x--; 
+				}
+				y--; 
+				x=y; 
+				if(let=='Z') {
+					let='A'; 
+				}
+				else {
+					let=(char)(let+1); 
+				}
+				output += " "; 
+			}
+			let = letter; 
+			x = amount; 
+			y = amount; 
+			output += "\n"; 
+		}
 		return output;
 	}
 }
